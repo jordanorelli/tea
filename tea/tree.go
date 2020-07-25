32e73e2626e077f8e76cbc9e6b26870f670fff12
Jordan Orelli <jordanorelli@gmail.com>
2020-07-25 16:27:35 +0000

    walk tree without that plan routine

--- tea/tree.go
+++ tea/tree.go
@@ -6,10 +6,30 @@ import (
 
 // Run runs a tree of tests, starting from its root.
 func Run(t *testing.T, tree *Tree) {
-	plan := tree.plan()
-	for _, step := range plan {
-		step.run(t)
-	}
+	t.Run(tree.name, func(t *testing.T) {
+		tree.Test.Run(t)
+
+		if t.Failed() || t.Skipped() {
+			for _, child := range tree.children {
+				skip(t, child)
+			}
+			return
+		}
+
+		for _, child := range tree.children {
+			tree.Test.Run(t)
+			Run(t, child)
+		}
+	})
+}
+
+func skip(t *testing.T, tree *Tree) {
+	t.Run(tree.name, func(t *testing.T) {
+		t.Skip("tea skipped: dependency failed")
+		for _, child := range tree.children {
+			skip(t, child)
+		}
+	})
 }
 
 func New(test Test) *Tree {
